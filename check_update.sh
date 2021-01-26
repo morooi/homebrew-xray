b9701bb3249202427c3f19b0d7973d2554b69bfc
morooi <19279567+morooi@users.noreply.github.com>
2021-01-26 20:35:20 +0800

    Update check_update.sh

--- check_update.sh
+++ check_update.sh
@@ -35,8 +35,8 @@ echo "start downloading..."
 curl -s -L "$INTEL_DOWNLOAD_URL" > Xray-macos-64.zip || { echo 'Intel version file download failed!' ; exit 1; }
 curl -s -L "$APPLE_SILICON_DOWNLOAD_URL" > Xray-macos-arm64-v8a.zip || { echo 'Apple Silicon version file download failed!' ; exit 1; }
 curl -s -L "https://raw.githubusercontent.com/XTLS/Xray-examples/main/VLESS-TCP-XTLS-WHATEVER/config_client/vless_tcp_xtls.json" > vless_tcp_xtls.json || { echo 'Config file download failed!' ; exit 1; }
-curl -s -L "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" > geoip.dat || { echo 'GeoIP.dat download failed!' ; exit 1; }
-curl -s -L "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" > geosite.dat || { echo 'GeoSite.dat download failed!' ; exit 1; }
+curl -s -L "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" > geoip.dat || { echo 'geoip.dat download failed!' ; exit 1; }
+curl -s -L "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" > geosite.dat || { echo 'geosite.dat download failed!' ; exit 1; }
 
 if [ ! -e Xray-macos-64.zip ]; then
     echo "Intel version file download failed!"
@@ -54,20 +54,20 @@ if [ ! -e vless_tcp_xtls.json ]; then
 fi
 
 if [ ! -e geoip.dat ]; then
-    echo "GeoIP.dat download failed!"
+    echo "geoip.dat download failed!"
     exit 1
 fi
 
 if [ ! -e geosite.dat ]; then
-    echo "GeoSite.dat download failed!"
+    echo "geosite.dat download failed!"
     exit 1
 fi
 
-INTEL_V_HASH256=$(sha256sum Xray-macos-64.zip |cut  -d ' ' -f 1)
-APPLE_SILICON_V_HASH256=$(sha256sum Xray-macos-arm64-v8a.zip |cut  -d ' ' -f 1)
-CONFIG_V_HASH256=$(sha256sum vless_tcp_xtls.json |cut  -d ' ' -f 1)
-GEOIP_V_HASH256=$(sha256sum geoip.dat |cut  -d ' ' -f 1)
-GEOSITE_V_HASH256=$(sha256sum geosite.dat |cut  -d ' ' -f 1)
+INTEL_V_HASH256=$(sha256sum Xray-macos-64.zip | cut  -d ' ' -f 1)
+APPLE_SILICON_V_HASH256=$(sha256sum Xray-macos-arm64-v8a.zip | cut  -d ' ' -f 1)
+CONFIG_V_HASH256=$(sha256sum vless_tcp_xtls.json | cut  -d ' ' -f 1)
+GEOIP_V_HASH256=$(sha256sum geoip.dat | cut  -d ' ' -f 1)
+GEOSITE_V_HASH256=$(sha256sum geosite.dat | cut  -d ' ' -f 1)
 
 echo "update xray.rb...."
 
