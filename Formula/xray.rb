d7414759f47b1d852b58846fd31b302fa2289c4a
actions <action@github.com>
2021-01-26 06:07:46 +0000

    Automated update xray-core version v1.2.3

--- Formula/xray.rb
+++ Formula/xray.rb
@@ -2,28 +2,28 @@ class Xray < Formula
   desc "Xray, Penetrates Everything. Also the best v2ray-core, with XTLS support. Fully compatible configuration."
   homepage "https://xtls.github.io/"
   if Hardware::CPU.intel?
-    url "https://github.com/XTLv1.2.3/Xray-macos-64.zip"
-    sha256 "247a5a84c3aecd5b88eb9fd171dfa9637ba739c" # Intel
+    url "https://github.com/XTLS/Xray-core/releases/download/v1.2.3/Xray-macos-64.zip"
+    sha256 "247a5a84c3aecd5b880108c1f7879d7795ad3dedb14eb9fd171dfa9637ba739c" # Intel
   else
-    url "https://githad/v1.2.3/Xray-macos-arm64-v8a.zip"
-    sha256 "20959a819672f4f712f399279d6cac6587d798dbed" # Apple Silicon
+    url "https://github.com/XTLS/Xray-core/releases/download/v1.2.3/Xray-macos-arm64-v8a.zip"
+    sha256 "20959a819672f4f712f39906e041caf4e60c90bee2a0279d6cac6587d798dbed" # Apple Silicon
   end
-  version "1.2.2"
+  version "1.2.3"
   license "MPL-2.0"
 
   resource "config" do
     url "https://raw.githubusercontent.com/XTLS/Xray-examples/main/VLESS-TCP-XTLS-WHATEVER/config_client/vless_tcp_xtls.json"
-    sha256 "1926e7e9bc7d84d8ef5783aec880564d2ad7a77" # Config
+    sha256 "1926e7e9bc7d84d8ef5783aec1dcd5c386b9c3e6cb36ad7adf880564d2ad7a77" # Config
   end
 
   resource "geoip" do
     url "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat"
-    sha256 "22d2e36eab00dde2346ecf9c9cff4787d41" # GeoIP
+    sha256 "22d2e36eab00dde2346ecf9c9cff328d69e1084bf8157323126d64be84787d41" # GeoIP
   end
 
   resource "geosite" do
     url "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat"
-    sha256 "6963ea663412768f4b9dcdb8d928e525535e29bfd" # GeoSite
+    sha256 "6963ea663412768f4b9dcdb8d928e525ab6b7894ba2cf5de80241f8535e29bfd" # GeoSite
   end
 
   def install
