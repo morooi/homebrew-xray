d014ae925977823e5d32bb531e479e1b5dd037cb
morooi <19279567+morooi@users.noreply.github.com>
2021-01-26 14:06:52 +0800

    更改了 GEOIP 和 GEOSITE 的地址

--- Formula/xray.rb
+++ Formula/xray.rb
@@ -2,20 +2,45 @@ class Xray < Formula
   desc "Xray, Penetrates Everything. Also the best v2ray-core, with XTLS support. Fully compatible configuration."
   homepage "https://xtls.github.io/"
   if Hardware::CPU.intel?
-    url "https://github.com/XTLS/Xray-core/releases/download/v1.2.3/Xray-macos-64.zip"
-    sha256 "247a5a84c3aecd5b880108c1f7879d7795ad3dedb14eb9fd171dfa9637ba739c" # Intel
+    url "https://github.com/XTLv1.2.3/Xray-macos-64.zip"
+    sha256 "247a5a84c3aecd5b88eb9fd171dfa9637ba739c" # Intel
   else
-    url "https://github.com/XTLS/Xray-core/releases/download/v1.2.3/Xray-macos-arm64-v8a.zip"
-    sha256 "20959a819672f4f712f39906e041caf4e60c90bee2a0279d6cac6587d798dbed" # Apple Silicon
+    url "https://githad/v1.2.3/Xray-macos-arm64-v8a.zip"
+    sha256 "20959a819672f4f712f399279d6cac6587d798dbed" # Apple Silicon
   end
-  version "1.2.3"
+  version "1.2.2"
   license "MPL-2.0"
 
+  resource "config" do
+    url "https://raw.githubusercontent.com/XTLS/Xray-examples/main/VLESS-TCP-XTLS-WHATEVER/config_client/vless_tcp_xtls.json"
+    sha256 "1926e7e9bc7d84d8ef5783aec880564d2ad7a77" # Config
+  end
+
+  resource "geoip" do
+    url "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat"
+    sha256 "22d2e36eab00dde2346ecf9c9cff4787d41" # GeoIP
+  end
+
+  resource "geosite" do
+    url "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat"
+    sha256 "6963ea663412768f4b9dcdb8d928e525535e29bfd" # GeoSite
+  end
+
   def install
-    bin.install "xray"    
-    pkgshare.install "geoip.dat"
-    pkgshare.install "geosite.dat"
-    (etc/"xray").mkpath
+    bin.install "xray"
+
+    resource("config").stage do
+      pkgetc.install "vless_tcp_xtls.json" => "config.json"
+    end
+
+    resource("geoip").stage do
+      pkgshare.install "geoip.dat"
+    end
+
+    resource("geosite").stage do
+      pkgshare.install "geosite.dat"
+    end
+
   end
 
   plist_options manual: "xray run -c #{HOMEBREW_PREFIX}/etc/xray/config.json"
@@ -27,12 +52,12 @@ class Xray < Formula
     <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
     <plist version="1.0">
       <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
         <key>KeepAlive</key>
         <true/>
         <key>RunAtLoad</key>
         <true/>
-        <key>Label</key>
-        <string>#{plist_name}</string>
         <key>ProgramArguments</key>
         <array>
           <string>#{bin}/xray</string>
--- check_update.sh
+++ check_update.sh
@@ -34,6 +34,9 @@ echo "start downloading..."
 
 curl -s -L "$INTEL_DOWNLOAD_URL" > Xray-macos-64.zip || { echo 'Intel version file download failed!' ; exit 1; }
 curl -s -L "$APPLE_SILICON_DOWNLOAD_URL" > Xray-macos-arm64-v8a.zip || { echo 'Apple Silicon version file download failed!' ; exit 1; }
+curl -s -L "https://raw.githubusercontent.com/XTLS/Xray-examples/main/VLESS-TCP-XTLS-WHATEVER/config_client/vless_tcp_xtls.json" > vless_tcp_xtls.json || { echo 'Config file download failed!' ; exit 1; }
+curl -s -L "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat" > geoip.dat || { echo 'GeoIP.dat download failed!' ; exit 1; }
+curl -s -L "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat" > geosite.dat || { echo 'GeoSite.dat download failed!' ; exit 1; }
 
 if [ ! -e Xray-macos-64.zip ]; then
     echo "Intel version file download failed!"
@@ -45,8 +48,26 @@ if [ ! -e Xray-macos-arm64-v8a.zip ]; then
     exit 1
 fi
 
+if [ ! -e vless_tcp_xtls.json ]; then
+    echo "Config file download failed!"
+    exit 1
+fi
+
+if [ ! -e geoip.dat ]; then
+    echo "GeoIP.dat download failed!"
+    exit 1
+fi
+
+if [ ! -e geosite.dat ]; then
+    echo "GeoSite.dat download failed!"
+    exit 1
+fi
+
 INTEL_V_HASH256=$(sha256sum Xray-macos-64.zip |cut  -d ' ' -f 1)
 APPLE_SILICON_V_HASH256=$(sha256sum Xray-macos-arm64-v8a.zip |cut  -d ' ' -f 1)
+CONFIG_V_HASH256=$(sha256sum vless_tcp_xtls.json |cut  -d ' ' -f 1)
+GEOIP_V_HASH256=$(sha256sum geoip.dat |cut  -d ' ' -f 1)
+GEOSITE_V_HASH256=$(sha256sum geosite.dat |cut  -d ' ' -f 1)
 
 echo "update xray.rb...."
 
@@ -55,6 +76,10 @@ sed -i "s#^\s*sha256.*Intel#    sha256 \"$INTEL_V_HASH256\" \# Intel#g" Formula/
 
 sed -i "s#^\s*url.*-arm64.*#    url \"$APPLE_SILICON_DOWNLOAD_URL\"#g" Formula/xray.rb
 sed -i "s#^\s*sha256.*Silicon#    sha256 \"$APPLE_SILICON_V_HASH256\" \# Apple Silicon#g" Formula/xray.rb
+
+sed -i "s#^\s*sha256.*Config#    sha256 \"$CONFIG_V_HASH256\" \# Config#g" Formula/xray.rb
+sed -i "s#^\s*sha256.*GeoIP#    sha256 \"$GEOIP_V_HASH256\" \# GeoIP#g" Formula/xray.rb
+sed -i "s#^\s*sha256.*GeoSite#    sha256 \"$GEOSITE_V_HASH256\" \# GeoSite#g" Formula/xray.rb
 
 sed -i "s#^\s*version.*#  version \"$V_VERSION\"#g" Formula/xray.rb
 
