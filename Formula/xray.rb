77903d91ec7a82d8afbdcdd1ec45e03473aaa7f5
actions <action@github.com>
2022-07-01 22:24:03 +0000

    Automated update resources

--- Formula/xray.rb
+++ Formula/xray.rb
@@ -23,7 +23,7 @@ class Xray < Formula
 
   resource "geosite" do
     url "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat"
-    sha256 "304a993e55fc2b426ffd9edd48cf580e354ac45efff29554d914bfc3c88b8715" # GeoSite
+    sha256 "e3199c77afbef1776debcc4b2192e76d832e57c475718fd5253577f8343bcbf2" # GeoSite
   end
 
   def install
