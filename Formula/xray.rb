7d2f4088512aad18782d152a66b4b72c4f564054
actions <action@github.com>
2022-12-20 07:54:29 +0000

    Automated update xray-core version v1.6.6

--- Formula/xray.rb
+++ Formula/xray.rb
@@ -2,13 +2,13 @@ class Xray < Formula
   desc "Xray, Penetrates Everything. Also the best v2ray-core, with XTLS support. Fully compatible configuration"
   homepage "https://xtls.github.io/"
   if Hardware::CPU.intel?
-    url "https://github.com/XTLS/Xray-core/releases/download/v/Xray-macos-64.zip"
+    url "https://github.com/XTLS/Xray-core/releases/download/v1.6.6/Xray-macos-64.zip"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Intel
   else
-    url "https://github.com/XTLS/Xray-core/releases/download/v/Xray-macos-arm64-v8a.zip"
+    url "https://github.com/XTLS/Xray-core/releases/download/v1.6.6/Xray-macos-arm64-v8a.zip"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5" # Apple Silicon
   end
-  version ""
+  version "1.6.6"
   license "MPL-2.0"
 
   resource "config" do
