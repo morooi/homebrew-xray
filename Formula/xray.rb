99b0ec0e628f0a771b7a77707e3bd27ffd7c9b2d
actions <action@github.com>
2026-08-13 23:19:17 +0000

    Automated update resources

--- Formula/xray.rb
+++ Formula/xray.rb
@@ -22,12 +22,12 @@ class Xray < Formula
 
   resource "geoip" do
     url "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geoip.dat"
-    sha256 "53dc90b50877d2ed974ea81fb0fbdcbac84e189c0ac7fdcab7a966bb5e36b6e6" # GeoIP
+    sha256 "b406dd3759037188b0674b110dcaf33664a699c0518152d0ca0d9023fc774c6b" # GeoIP
   end
 
   resource "geosite" do
     url "https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/geosite.dat"
-    sha256 "034f0bbbe3bf030c603a2b61cefb7d72c95eaa0aa3b1705edd71c9ef25f56847" # GeoSite
+    sha256 "174c0a0571a86c9addff6c39f240d28fb1dd1643a7a969fd7b47cbfbbd1e8a95" # GeoSite
   end
 
   def install
